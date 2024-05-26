af6a67a90e9cc43996611bec3a4412d798d21e09
kwasniewskin <nikodem.kwasniewski.2@gmail.com>
2024-05-26 12:31:35 +0200

    GometryCalculator and MinMaxArray finished

---
+++ StudiesSimpleConsolePrograms/MinMaxArray.cpp/MinMaxArray.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    float tablica[10];
+    float suma = 0.0;
+    float najwieksza;
+    float najmniejsza;
+
+    cout << "Podaj 10 liczb:" << endl;
+    for (int i = 0; i < 10; ++i) {
+        cout << "Liczba " << i + 1 << ": ";
+        cin >> tablica[i];
+        suma += tablica[i];
+    }
+
+    najwieksza = tablica[0];
+    for (int i = 1; i < 10; ++i) {
+        if (tablica[i] > najwieksza) {
+            najwieksza = tablica[i];
+        }
+    }
+
+    najmniejsza = tablica[0];
+    for (int i = 1; i < 10; ++i) {
+        if (tablica[i] < najmniejsza) {
+            najmniejsza = tablica[i];
+        }
+    }
+
+    cout << "Najwieksza wartosc: " << najwieksza << endl;
+    cout << "Najmniejsza wartosc: " << najmniejsza << endl;
+
+    return 0;
+}
---
+++ StudiesSimpleConsolePrograms/StudiesSimpleConsolePrograms/geometryCalculator.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float ObliczPoleKwadratu(float bokA) {
+    return bokA * bokA;
+}
+
+float ObliczPoleProstokata(float bokA, float bokB) {
+    return bokA * bokB;
+}
+
+float ObliczPoleTrojkata(float podstawaA, float wysokoscNaA) {
+    return 0.5 * podstawaA * wysokoscNaA;
+}
+
+float ObliczObwodKwadratu(float bokA) {
+    return 4 * bokA;
+}
+
+float ObliczObwodProstokata(float bokA, float bokB) {
+    return 2 * (bokA + bokB);
+}
+
+float ObliczObwodTrojkata(float bokA, float bokB, float bokC) {
+    return bokA + bokB + bokC;
+}
+
+int main() {
+    char wybor;
+    char figura;
+    float a, b, c, h;
+
+    do {
+        cout << "Co chcesz obliczyc?\n";
+        cout << " o = obwod\n p = pole \n q = wyjdz \n";
+        cin >> wybor;
+        if (wybor != 'q') {
+            cout << "Dla jakiej figury chcesz to obliczyc? (t = trojkat, p = prostokat, k = kwadrat)" << endl;
+            cin >> figura;
+        }
+
+        switch (figura) {
+        case 't':
+            switch (wybor) {
+            case 'o':
+                cout << "Podaj 3 boki trojkata: " << endl;
+                cin >> a >> b >> c;
+                cout << "Obwod podanego trojkata to: " << ObliczObwodTrojkata(a, b, c) << endl;
+                break;
+            case 'p':
+                cout << "Podaj podstawe i wysokosc trojkata" << endl;
+                cout << "Podstawa: ";
+                cin >> a;
+                cout << "Wysokosc: ";
+                cin >> h;
+                cout << "Pole podanego trojkata to: " << ObliczPoleTrojkata(a, h) << endl;
+                break;
+            }
+            break;
+        case 'p':
+            switch (wybor) {
+            case 'o':
+                cout << "Podaj dlugosci bokow prostokata" << endl;
+                cin >> a >> b;
+                cout << "Obwod prostokata to: " << ObliczObwodProstokata(a, b) << endl;
+                break;
+            case 'p':
+                cout << "Podaj dlugosci bokow prostokata" << endl;
+                cout << "Dlugosc boku A: ";
+                cin >> a;
+                cout << "Dlugosc boku B: ";
+                cin >> b;
+                cout << "Pole prostokata to: " << ObliczPoleProstokata(a, b) << endl;
+                break;
+            }
+            break;
+        case 'k':
+            switch (wybor) {
+            case 'o':
+                cout << "Podaj dlugosc boku kwadratu" << endl;
+                cin >> a;
+                cout << "Obwod kwadratu to: " << ObliczObwodKwadratu(a) << endl;
+                break;
+            case 'p':
+                cout << "Podaj dlugosc boku kwadratu" << endl;
+                cout << "Dlugosc boku: ";
+                cin >> a;
+                cout << "Pole kwadratu to: " << ObliczPoleKwadratu(a) << endl;
+                break;
+            }
+            break;
+        default:
+            cout << "Wprowadzono niepoprawne dane!";
+            break;
+        }
+    } while (wybor != 'q');
+
+    return 0;
+}
